a0d697e3224900f6f6652b6d49b41ee3ecda410e
Peter Johnson <peter.d.johnson22@gmail.com>
2021-04-01 10:59:29 -0400

    Adding typing and removing print statements

--- src/python/zquantum/featureselection/feature_selection.py
+++ src/python/zquantum/featureselection/feature_selection.py
@@ -3,7 +3,7 @@ from cvxopt import matrix, solvers
 from scipy.stats import pearsonr
 from scipy.optimize import minimize, LinearConstraint
 from sklearn.feature_selection import mutual_info_classif, mutual_info_regression
-from typing import List, Optional, Union
+from typing import List, Optional, Union, Tuple
 
 solvers.options["show_progress"] = False
 
@@ -67,16 +67,16 @@ def construct_mutual_info_relevance_vector(
     return relevance_vector
 
 
-def construct_pearson_corr_redundancy_matrix(feature_matrix):
+def construct_pearson_corr_redundancy_matrix(feature_matrix: np.ndarray) -> np.ndarray:
     """Constructs the Pearson correlation redundancy matrix of a feature matrix.
         This follows the construction of rho_ij in eq. 3 of 'Rodriguez-Lujan, Irene, et al. "Quadratic programming
         feature selection." Journal of Machine Learning Research (2010).'
 
     Args:
-        feature_matrix (np.ndarray): 2D array with features along the columns and data points along the rows.
+        feature_matrix: 2D array with features along the columns and data points along the rows.
 
     Returns:
-        redundancy_matrix (np.ndarray): 2D square array assigning a Pearson correlation score to each pair of features.
+        redundancy_matrix: 2D square array assigning a Pearson correlation score to each pair of features.
     """
 
     redundancy_matrix = np.abs(np.corrcoef(feature_matrix.T))
@@ -84,16 +84,18 @@ def construct_pearson_corr_redundancy_matrix(feature_matrix):
     return redundancy_matrix
 
 
-def construct_mutual_info_redundancy_matrix(feature_matrix, seed=None):
+def construct_mutual_info_redundancy_matrix(
+    feature_matrix: np.ndarray, seed: Union[int, None] = None
+) -> np.ndarray:
     """Constructs the mutual information redundancy matrix of a feature matrix.
     See https://scikit-learn.org/stable/modules/generated/sklearn.feature_selection.mutual_info_regression.html
 
     Args:
-        feature_matrix (np.ndarray): 2D array with features along the columns and data points along the rows.
-        seed (int): random seed.
+        feature_matrix: 2D array with features along the columns and data points along the rows.
+        seed: random seed.
 
     Returns:
-        redundancy_matrix (np.ndarray): 2D square array assigning a mutual information score to each pair of features.
+        redundancy_matrix: 2D square array assigning a mutual information score to each pair of features.
     """
     if seed is not None:
         np.random.seed(seed=seed)
@@ -112,18 +114,18 @@ def construct_mutual_info_redundancy_matrix(feature_matrix, seed=None):
 
 
 def _weight_features_with_quadratic_programming(
-    redundancy_matrix, relevance_vector, alpha
-):
+    redundancy_matrix: np.ndarray, relevance_vector: np.ndarray, alpha: float
+) -> np.ndarray:
     """Assigns an importance weight to each feature according to the output of a quadratic program following the methodology of
     'Rodriguez-Lujan, Irene, et al. "Quadratic programming feature selection." Journal of Machine Learning Research (2010).'
 
     Args:
-        redundancy_matrix (np.ndarray): 2D square array assigning a correlation score to each pair of features.
-        relevance_vector (np.ndarray): 1D array assigning a relevance score to each feature.
-        alpha (float): parameter between 0 and 1 which weights the importance of relevance (towards 1) vs redundancy (towards 0).
+        redundancy_matrix: 2D square array assigning a correlation score to each pair of features.
+        relevance_vector: 1D array assigning a relevance score to each feature.
+        alpha: parameter between 0 and 1 which weights the importance of relevance (towards 1) vs redundancy (towards 0).
 
     Returns:
-        weight_vector (np.ndarray): 1D array assigning an importance weight to each feature.
+        weight_vector: 1D array assigning an importance weight to each feature.
     """
 
     num_of_features = len(relevance_vector)
@@ -140,20 +142,23 @@ def _weight_features_with_quadratic_programming(
 
 
 def quadratic_programming_feature_selection(
-    redundancy_matrix, relevance_vector, num_of_chosen_features, alpha=None
-):
+    redundancy_matrix: np.ndarray,
+    relevance_vector: np.ndarray,
+    num_of_chosen_features: int,
+    alpha: Union[float, None] = None,
+) -> Tuple[List[int], np.ndarray]:
     """Selects a subset of features based on a quadratic program following the methodology of
     'Rodriguez-Lujan, Irene, et al. "Quadratic programming feature selection." Journal of Machine Learning Research (2010).'
 
     Args:
-        redundancy_matrix (np.ndarray): 2D square array assigning a correlation score to each pair of features.
-        relevance_vector (np.ndarray): 1D array assigning a relevance score to each feature.
-        num_of_chosen_features (int): number of features to be selected
-        alpha (float): parameter between 0 and 1 which weights the importance of relevance (towards 1) vs redundancy (towards 0).
+        redundancy_matrix: 2D square array assigning a correlation score to each pair of features.
+        relevance_vector: 1D array assigning a relevance score to each feature.
+        num_of_chosen_features: number of features to be selected
+        alpha: parameter between 0 and 1 which weights the importance of relevance (towards 1) vs redundancy (towards 0).
 
     Returns:
-        chosen_ones (list): list of integers indexing the selected features.
-        weight_vector (np.ndarray): 1D array assigning an importance weight to each of the original features.
+        chosen_ones: list of integers indexing the selected features.
+        weight_vector: 1D array assigning an importance weight to each of the original features.
     """
     # Ensure that the redundancy matrix is symmetric
     redundancy_matrix = (redundancy_matrix + redundancy_matrix.T) / 2.0
@@ -176,20 +181,23 @@ def quadratic_programming_feature_selection(
 
 
 def greedy_mrmr_feature_selection(
-    redundancy_matrix, relevance_vector, num_of_chosen_features, seed=None
-):
+    redundancy_matrix: np.ndarray,
+    relevance_vector: np.ndarray,
+    num_of_chosen_features: int,
+    seed: Union[int, None] = None,
+) -> Tuple[set, float, float, float]:
     """Selects a subset of features based on a the Minimum Redundancy Maximum Relevance method found here:
     https://en.wikipedia.org/wiki/Feature_selection#Minimum-redundancy-maximum-relevance_(mRMR)_feature_selection
 
     Args:
-        redundancy_matrix (np.ndarray): 2D square array assigning a correlation score to each pair of features.
-        relevance_vector (np.ndarray): 1D array assigning a relevance score to each feature.
-        num_of_chosen_features (int): number of features to be selected
-        seed (int): random seed.
+        redundancy_matrix: 2D square array assigning a correlation score to each pair of features.
+        relevance_vector: 1D array assigning a relevance score to each feature.
+        num_of_chosen_features: number of features to be selected
+        seed: random seed.
 
     Returns:
-        chosen_ones (set): set of integers indexing the selected features.
-        mrmr (float): final mrmr score of selected subset of features.
+        chosen_ones: set of integers indexing the selected features.
+        mrmr: final mrmr score of selected subset of features.
         avg_relevance: average relevance score of selected subset of features.
         avg_redundancy: average redundancy score of selected subset of features.
     """
@@ -247,14 +255,16 @@ def greedy_mrmr_feature_selection(
 
 
 def generate_reduced_quadratic_program_with_qpfs(
-    redundancy_matrix,
-    relevance_vector,
-    num_active_features,
-    constant_contribution=np.zeros(1),
-    num_frozen_features=0,
-    alpha=None,
-    frozen_vector_strategy="QUBO",
-):
+    redundancy_matrix: np.ndarray,
+    relevance_vector: np.ndarray,
+    num_active_features: int,
+    constant_contribution: np.ndarray = np.zeros(1),
+    num_frozen_features: int = 0,
+    alpha: Union[float, None] = None,
+    frozen_vector_strategy: str = "QUBO",
+) -> Tuple[
+    np.ndarray, np.ndarray, np.ndarray, List[int], List[int], List[int], np.ndarray, str
+]:
     """Generates a reduced instance of the quadratic programming feature selection problem. Splits features into frozen, active, and deselected
     corresponding respectively to those automatically chosen, those to be used in the reduced instance, and those to be automatically deselected.
     The reduced instances are constructed by evaluating certain entries in the optimization argument x to yield an effective constrained instance:
@@ -318,9 +328,6 @@ def generate_reduced_quadratic_program_with_qpfs(
         redundancy_matrix, relevance_vector, num_features, alpha=alpha
     )
 
-    # print("redundancy_matrix", redundancy_matrix)
-    # print("relevance_vector", relevance_vector)
-    # print("feature_weights", feature_weights)
     # Check to make sure that num_frozen + num_selected is less than total
     if num_active_features + num_frozen_features > num_features:
         raise ValueError(
@@ -336,7 +343,6 @@ def generate_reduced_quadratic_program_with_qpfs(
     # For each rank (0 is lowest), list entry is the feature index
     # sorted_features = np.argsort(feature_weights)[::-1]
     sorted_features = np.argsort(feature_weights)
-    # print("sorted_features", sorted_features)
 
     # Get lists of frozen, active, and deselected features
     num_deselected_features = num_features - (num_active_features + num_frozen_features)
@@ -345,7 +351,6 @@ def generate_reduced_quadratic_program_with_qpfs(
         num_deselected_features : num_deselected_features + num_active_features
     ]
     frozen_features = sorted_features[num_deselected_features + num_active_features :]
-    # print(deselected_features, active_features, frozen_features)
 
     # Get the effective redundancy matrix and relevance vector
     effective_redundancy_matrix = (1 - alpha) * redundancy_matrix
@@ -355,7 +360,6 @@ def generate_reduced_quadratic_program_with_qpfs(
     reduced_redundancy_matrix = effective_redundancy_matrix[
         np.ix_(active_features, active_features)
     ]
-    # print("reduced_redundancy_matrix", reduced_redundancy_matrix)
 
     ### Generate frozen weights vector according to strategy
     if frozen_vector_strategy == "QUBO":
@@ -377,41 +381,31 @@ def generate_reduced_quadratic_program_with_qpfs(
             f"Frozen vector strategy {frozen_vector_strategy} not currently supported. Please select consult doc strings for valid options."
         )
 
-    # print("frozen_weights_vector", frozen_weights_vector)
-
     ### Compute reduced relevance vector
     frozen_active_redundancy_matrix = effective_redundancy_matrix[
         np.ix_(frozen_features, active_features)
     ]
-    # print("frozen_active_redundancy_matrix", frozen_active_redundancy_matrix)
     frozen_redundancy_contribution_vector = (
         frozen_weights_vector @ frozen_active_redundancy_matrix
     )
-    # print("frozen_redundancy_contribution_vector", frozen_redundancy_contribution_vector)
     reduced_relevance_vector = (
         frozen_redundancy_contribution_vector
         + effective_relevance_vector[active_features]
     )
-    # print("reduced_relevance_vector", reduced_relevance_vector)
 
-    ### TODO: Alternatively, add constant term to diagonal of redundandy matrix?
+    ### TODO: Alternatively, add constant term to diagonal of redundandy matrix
     frozen_redundancy_matrix = effective_redundancy_matrix[
         np.ix_(frozen_features, frozen_features)
     ]
-    # print("frozen_redundancy_matrix", frozen_redundancy_matrix)
     frozen_relevance_vector = effective_relevance_vector[frozen_features]
-    # print("frozen_relevance_vector", frozen_relevance_vector)
     constant_from_frozen_redundancy = (
         np.dot(frozen_weights_vector, frozen_redundancy_matrix @ frozen_weights_vector)
         * 0.5
     )
-    # print("constant_from_frozen_redundancy", constant_from_frozen_redundancy)
     constant_from_frozen_relevancy = frozen_weights_vector @ frozen_relevance_vector
-    # print("constant_from_frozen_relevancy", constant_from_frozen_relevancy)
     constant_contribution = (
         constant_from_frozen_redundancy + constant_from_frozen_relevancy
     )
-    # print("constant_contribution", constant_contribution)
 
     return (
         reduced_redundancy_matrix,
